8cccf07e4f896f4201b4b3ba9ae41355d3882b82
romeda <romeda@f7ae4463-c52f-0410-a7dc-93bad6e629e8>
2007-11-15 11:01:12 +0000

    proper quoting for hash keys and add a few more oauth accessors.

--- code/ruby/oauth/lib/oauth/request_proxy/base.rb
+++ code/ruby/oauth/lib/oauth/request_proxy/base.rb
@@ -8,21 +8,33 @@ module OAuth::RequestProxy
 
     attr_accessor :request, :options
 
-    def initialize(request, options)
+    def initialize(request, options = {})
       @request = request
       @options = options
     end
 
     def token
-      parameters[:oauth_token]
+      parameters['oauth_token']
     end
 
     def consumer_key
-      parameters[:oauth_consumer_key]
+      parameters['oauth_consumer_key']
+    end
+
+    def nonce
+      parameters['oauth_nonce']
+    end
+
+    def timestamp
+      parameters['oauth_timestamp']
+    end
+
+    def signature_method
+      parameters['oauth_signature_method']
     end
 
     def signature
-      parameters[:oauth_signature]
+      parameters['oauth_signature']
     end
   end
 end
