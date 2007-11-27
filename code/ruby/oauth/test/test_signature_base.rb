be88b430d6829868c3a99a948716d4fb46b199cb
romeda <romeda@f7ae4463-c52f-0410-a7dc-93bad6e629e8>
2007-11-27 06:26:14 +0000

    make some more tests pass. still needs work.

--- code/ruby/oauth/test/test_signature_base.rb
+++ code/ruby/oauth/test/test_signature_base.rb
@@ -11,16 +11,21 @@ class SignatureBaseTest < Test::Unit::TestCase
 
   def test_that_initialize_requires_a_valid_request_argument
     request = nil
-    assert_raises OAuth::RequestProxy::UnknownRequestType do
-      OAuth::Signature::Base.new(request)
+    assert_raises TypeError do
+      OAuth::Signature::Base.new(request) { |token|
+        # just a stub
+      }
     end
   end
 
   def test_that_initialize_succeeds_when_the_request_proxy_is_valid
     # this isn't quite valid, but it will do.
-    request = OAuth::RequestProxy::Base.new(nil, nil)
+    raw_request = Net::HTTP::Get.new('/test')
+    request = OAuth::RequestProxy.proxy(raw_request)
     assert_nothing_raised do
-      OAuth::Signature::Base.new(request)
+      OAuth::Signature::Base.new(request) { |token|
+        # just a stub
+      }
     end
   end
 
