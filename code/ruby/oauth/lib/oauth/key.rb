4808c9c333970e85de2c861eaac7674cfd294994
pelleb <pelleb@f7ae4463-c52f-0410-a7dc-93bad6e629e8>
2008-01-08 19:06:24 +0000

    got rid of escape in Key module

--- code/ruby/oauth/lib/oauth/key.rb
+++ code/ruby/oauth/lib/oauth/key.rb
@@ -4,12 +4,6 @@ module OAuth
   module Key
     def generate_key(size=32)
       Base64.encode64(OpenSSL::Random.random_bytes(size)).gsub(/\W/,'')
-    end
-    
-    # Based on Blaine's example from the Oauth mailing list
-    def escape(value)
-      CGI.escape(value.to_s).gsub("%7E", "~").gsub("+", "%20")
-    end
-    
+    end    
   end
 end
