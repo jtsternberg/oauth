6a0ceb9310a909508424c41b78fb4ad3b4ac4cb8
seth@mojodna.net <seth@mojodna.net@f7ae4463-c52f-0410-a7dc-93bad6e629e8>
2008-09-03 20:44:08 +0000

    moved stuff to http://github.com/mojodna/sample-oauth-provider/tree/master

--- code/ruby/sample_oauth_provider/app/models/oauth_access_token.rb
+++
@@ -1,5 +0,0 @@
-class OauthAccessToken < OauthToken
-  def access_token?
-    true
-  end
-end
--- code/ruby/sample_oauth_provider/config/initializers/oauth.rb
+++
@@ -1,7 +0,0 @@
-require 'oauth/signature/hmac/sha1'
-require 'oauth/signature/plaintext'
-require 'oauth/request_proxy/action_controller_request'
-
-class OAuth::InvalidRequest < Exception; end
-class OAuth::AccessTokenRequired < Exception; end
-class OAuth::RequestTokenRequired < Exception; end
